ac8889b7b7b12a0db10c289c00851ee7ac0230ef
prasannajitsahoo <42238651+prasannajitsahoo@users.noreply.github.com>
2018-11-04 21:52:57 +0530

    Add files via upload

---
+++ lab9_q5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   char s[10] ="abcde";
+   char* cptr;
+   cptr=s;
+   //using the loops
+   for(int i=10;i>=0;i--)
+   {
+       cout<<*(cptr+i)<<endl;
+   }
+   return 0;
+}
+
